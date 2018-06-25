1c589358bca194f5a4fd6723575fa52d730eff39
vain0 <id_vain@yahoo.co.jp>
2018-06-25 22:56:33 +0900

    Move header/footer to surface-frame-component

--- try-friends/src/client/views/surface-frame-component.vue
+++ try-friends/src/client/views/surface-frame-component.vue
@@ -1,30 +1,28 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <router-link
-            to="/"
-          >Ok
-          </router-link>
-        </li>
-        <li>
-          <router-link
-            to="/about/"
-          >About
-          </router-link>
-        </li>
-      </ul>
-    </nav>
+  <article id="main-content">
+    <header>
+      <img
+        id="header-logo"
+        width="874px"
+        height="350px"
+        alt="logo"
+        src="/assets/logo.png"
+      >
+    </header>
 
     <main id="vue-router">
       <router-view/>
     </main>
-  </div>
+
+    <footer>
+      Try Friends-lang powered by
+        <a href="https://aratama.github.io/kemonogen/">Kemono Friends Logo Generator</a>
+        and <a href="https://www.flickr.com/photos/5lab/4004317022/">Savanna</a>.
+    </footer>
+  </article>
 </template>
 
 <style lang="scss">
-
 </style>
 
 <script lang="ts">
