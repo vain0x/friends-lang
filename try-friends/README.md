047ee86a36649238600fc1d41f8414ce0bd5dd30
vain0x <vainzerox@gmail.com>
2021-10-31 18:30:56 +0900

    feat: Remove unfinished 'try-friends' project

--- try-friends/README.md
+++
@@ -1,9 +0,0 @@
-# try-friends
-
-*Under development*
-
-Web app to try Friends-lang.
-
-## See also
-
-See [CONTRIBUTING](CONTRIBUTING.md) for development.
--- try-friends/src/core/awesome.spec.ts
+++
@@ -1,8 +0,0 @@
-import { message } from './awesome';
-import { TestSuite } from './testing-types';
-
-export const testSuite: TestSuite = ({ describe, context, it, eq }) => {
-  it('message', () => {
-    eq(true, message().indexOf('Hello') >= 0);
-  });
-};
